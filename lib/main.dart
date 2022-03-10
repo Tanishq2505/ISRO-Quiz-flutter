b2d584f4151386c28d61708e6e8d8f8e9e1ea460
Tanishq Kashyap <tanishqa89@gmail.com>
2022-03-11 00:56:40 +0530

    added multiple routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:isro_quiz/constants.dart';
+import 'package:isro_quiz/screens/centres_dashboard.dart';
 import 'package:isro_quiz/screens/dashboard_screen.dart';
+import 'package:isro_quiz/screens/launchers_dashboard.dart';
 import 'package:isro_quiz/screens/login_screen.dart';
+import 'package:isro_quiz/screens/questions_screen.dart';
+import 'package:isro_quiz/screens/quiz_dashboard.dart';
 import 'package:isro_quiz/screens/registration_screen.dart';
+import 'package:isro_quiz/screens/satellite_dashboard.dart';
+import 'package:isro_quiz/screens/score_screen.dart';
+import 'package:isro_quiz/screens/spacecrafts_dashboard.dart';
 import 'package:isro_quiz/screens/study_dashboard.dart';
 import 'package:isro_quiz/screens/welcome_screen.dart';
 
@@ -14,7 +23,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'ISRO Quiz',
       initialRoute: WelcomeScreen.id,
       routes: {
@@ -23,6 +32,15 @@ class MyApp extends StatelessWidget {
         RegistrationScreen.id: (context) => RegistrationScreen(),
         DashboardScreen.id: (context) => DashboardScreen(),
         StudyDashboard.id: (context) => StudyDashboard(),
+        SpacecraftDashboard.id: (context) => SpacecraftDashboard(),
+        LaunchersDashboard.id: (context) => LaunchersDashboard(),
+        SatelliteDashboard.id: (context) => SatelliteDashboard(),
+        CentresDashboard.id: (context) => CentresDashboard(),
+        QuizDashboard.id: (context) => QuizDashboard(),
+        QuizScreen.id: (context) => QuizScreen(),
+        ScoreScreen.id: (context) => ScoreScreen(
+              subject: kSubject.sub1,
+            ),
       },
     );
   }
